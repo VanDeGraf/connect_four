b5968bb85ee5f1dca527464fefac9f4e77bdab6d
VanDeGraf <vandegraf@bk.ru>
2021-05-17 19:10:06 +0400

    Commit for question

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,8 +1,8 @@
 class ConnectFour
   def initialize
-    @board = nil
+    @board = [[]]
     @winner = nil
-    @player_names = nil
+    @player_names = []
     @current_player = 0
   end
 
@@ -24,5 +24,94 @@ class ConnectFour
     else
       puts "Player #{@player_name[@winner]} Win the game!"
     end
+  end
+
+  def player_name_input
+    input = gets.chomp
+    until input.length >= 3
+      puts "Wrong input! Name length must more than 2! Type right: "
+      input = gets.chomp
+    end
+    input
+  end
+
+  def board_size_input
+    input = gets.chomp
+    until input.match?(/^\d+$/) && input.to_i >= 4
+      puts "Wrong input! Type integer less then 4: "
+      input = gets.chomp
+    end
+    input.to_i
+  end
+
+  def introdution
+    puts "Welcome to Connect Four game!"
+
+    puts "Player 1, say your name: "
+    @player_names << player_name_input
+    puts "Player 2, say your name: "
+    @player_names << player_name_input
+
+    puts "Type board width: "
+    width = board_size_input
+    puts "Type board height: "
+    height = board_size_input
+    @board = Array.new(height) { Array.new(width, nil) }
+
+    print_game_status
+  end
+
+  def player_turn_input
+    input = gets.chomp
+    until input.match?(/^\d+ \d+$/) &&
+          input.split.map { |s| s.to_i }[0].between?(0, @board[0].length) &&
+          input.split.map { |s| s.to_i }[1].between?(0, @board.length)
+      puts "Wrong input! Type x,y coordinate on board: "
+      input = gets.chomp
+    end
+    input.split.map { |s| s.to_i }
+  end
+
+  def player_turn
+    puts "Player #{@player_name[@current_player]} turn. Type x,y coordinate on board: "
+    x, y = player_turn_input
+    until @board[y][x].nil?
+      puts "Coordinate is already occupied! Type another: "
+      x, y = player_turn_input
+    end
+    @board[y][x] = @current_player
+    @current_player = @current_player == 0 ? 1 : 0
+  end
+
+  def count_elements_in_row_from_point(x, y)
+    point_player = @board[y][x]
+    return 0 if point_player.nil?
+    axis = {
+      vertical: 0,
+      horizontal: 0,
+      left_right_diagonal: 0,
+      right_left_diagonal: 0,
+    }
+    axis[:vertical] += count_line_elements(x, y) { |xx, yy| [xx, yy + 1] }
+    axis[:vertical] += count_line_elements(x, y) { |x, y| [x, y - 1] }
+    axis[:horizontal] += count_line_elements(x, y) { |x, y| [x + 1, y] }
+    axis[:horizontal] += count_line_elements(x, y) { |x, y| [x - 1, y] }
+    axis[:left_right_diagonal] += count_line_elements(x, y) { |x, y| [x + 1, y + 1] }
+    axis[:left_right_diagonal] += count_line_elements(x, y) { |x, y| [x - 1, y - 1] }
+    axis[:right_left_diagonal] += count_line_elements(x, y) { |x, y| [x + 1, y - 1] }
+    axis[:right_left_diagonal] += count_line_elements(x, y) { |x, y| [x - 1, y + 1] }
+    axis.max
+  end
+
+  def count_line_elements(x, y)
+    point_player = @board[y][x]
+    target_player = point_player
+    accumulator = 0
+    while point_player == target_player
+      accumulator += 1
+      x, y = yield(x, y)
+      target_player = @board.dig(y, x)
+    end
+    accumulator
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,26 +4,26 @@ describe ConnectFour do
   subject(:game) { described_class.new }
 
   describe "#initialize" do
-    # Don't need testing because only init variables    
+    # Don't need testing because only inxit variables
   end
 
   describe "#print_game_status" do
     # Don't need testing because puts only
-    it "defined" do
+    xit "defined" do
       expect(game).to respond_to(:print_game_status)
     end
   end
 
   describe "#print_game_result" do
     # Don't need testing because puts only
-    it "defined" do
+    xit "defined" do
       expect(game).to respond_to(:print_game_result)
     end
   end
 
   describe "#play_game" do
     # Don't need testing because contains only testing methods
-    it "defined" do
+    xit "defined" do
       expect(game).to respond_to(:play_game)
     end
   end
@@ -32,19 +32,20 @@ describe ConnectFour do
     context "when input string is empty, and then is valid" do
       before do
         allow(game).to receive(:gets).and_return("\n", "Bob\n")
+        allow(game).to receive(:puts)
       end
 
-      it "puts there is input is empty" do
+      xit "puts there is input is wrong" do
         expect(game).to receive(:puts).at_least(:once)
         game.player_name_input
       end
 
-      it "gets twice (wrong and not) input" do
+      xit "gets twice (wrong and not) input" do
         expect(game).to receive(:gets).twice
         game.player_name_input
       end
 
-      it "return valid" do
+      xit "return valid" do
         expect(game.player_name_input).to eq("Bob")
       end
     end
@@ -54,29 +55,31 @@ describe ConnectFour do
     context "when input is invalid, and then is valid" do
       before do
         allow(game).to receive(:gets).and_return("aksdasd\n", "7\n")
+        allow(game).to receive(:puts)
       end
 
-      it "puts there is wrong input once" do
+      xit "puts there is wrong input once" do
         expect(game).to receive(:puts).at_least(:once)
         game.board_size_input
       end
 
-      it "gets on wrong and valid inputs" do
+      xit "gets on wrong and valid inputs" do
         expect(game).to receive(:gets).twice
         game.board_size_input
       end
 
-      it "return valid input" do
+      xit "return valid input" do
         expect(game.board_size_input).to be 7
       end
     end
 
     context "when input is less then 4" do
       before do
-        allow(game).to receive(:gets).and_return("3\n")
+        allow(game).to receive(:gets).and_return("3\n", "7\n")
+        allow(game).to receive(:puts)
       end
 
-      it "puts there is wrong input" do
+      xit "puts there is wrong input" do
         expect(game).to receive(:puts).at_least(:once)
         game.board_size_input
       end
@@ -88,125 +91,129 @@ describe ConnectFour do
       allow(game).to receive(:player_name_input).and_return("Bob", "Anna")
       allow(game).to receive(:board_size_input).and_return(7, 6)
       allow(game).to receive(:print_game_status)
+      allow(game).to receive(:puts)
     end
 
-    it "calls #player_name_input twice" do
+    xit "calls #player_name_input twice" do
       expect(game).to receive(:player_name_input).twice
       game.introdution
     end
 
-    it "set player names" do
-      expect { game.introdution }.to change { game.instance_variable_gets(:@player_names) }.to(["Bob", "Anna"])
+    xit "set player names" do
+      expect { game.introdution }.to change { game.instance_variable_get(:@player_names) }.to(["Bob", "Anna"])
     end
 
-    it "calls #board_size_input twice" do
+    xit "calls #board_size_input twice" do
       expect(game).to receive(:board_size_input).twice
       game.introdution
     end
 
-    it "creates board array with inputed length" do
+    xit "creates board array with inputed length" do
       expect { game.introdution }.to change {
-                                       game.instance_variable_gets(:@board).length
+                                       game.instance_variable_get(:@board).length
                                      }.to(6).and change {
-                                       game.instance_variable_gets(:@board)[0].length
+                                       game.instance_variable_get(:@board)[0].length
                                      }.to(7)
     end
 
-    it "calls #print_game_status once" do
+    xit "calls #print_game_status once" do
       expect(game).to receive(:print_game_status).once
       game.introdution
     end
   end
 
   describe "#player_turn_input" do
+    before do
+      allow(game).to receive(:puts)
+    end
     context "when input empty string" do
       before do
-        allow(game).to receive(:gets).and_return("\n")
+        allow(game).to receive(:gets).and_return("\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input once" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
 
     context "when input hasn't space" do
       before do
-        allow(game).to receive(:gets).and_return("1\n")
+        allow(game).to receive(:gets).and_return("1\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when input has space more then 1" do
       before do
-        allow(game).to receive(:gets).and_return("1 1 2\n")
+        allow(game).to receive(:gets).and_return("1 1 2\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when input x and y coordinate not integer" do
       before do
-        allow(game).to receive(:gets).and_return("a5 b10\n")
+        allow(game).to receive(:gets).and_return("a5 b10\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when input x coordinate is integer, but y not" do
       before do
-        allow(game).to receive(:gets).and_return("5 b10\n")
+        allow(game).to receive(:gets).and_return("5 b10\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when input y coordinate is integer, but x not" do
       before do
-        allow(game).to receive(:gets).and_return("a5 10\n")
+        allow(game).to receive(:gets).and_return("a5 10\n", "0 0\n")
       end
 
-      it "puts wrong input" do
-        expect(game).to receive(:puts).to be_include("wrong input")
+      xit "puts wrong input" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when x coordinate out of board" do
       before do
-        allow(game).to receive(:gets).and_return("10 5\n")
+        allow(game).to receive(:gets).and_return("10 5\n", "0 0\n")
       end
 
-      it "puts coordinate out of board" do
-        expect(game).to receive(:puts).to be_include("coordinate out of board")
+      xit "puts coordinate out of board" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when y coordinate out of board" do
       before do
-        allow(game).to receive(:gets).and_return("5 10\n")
+        allow(game).to receive(:gets).and_return("5 10\n", "0 0\n")
       end
 
-      it "puts coordinate out of board" do
-        expect(game).to receive(:puts).to be_include("coordinate out of board")
+      xit "puts coordinate out of board" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
     context "when x and y coordinate out of board" do
       before do
-        allow(game).to receive(:gets).and_return("15 10\n")
+        allow(game).to receive(:gets).and_return("15 10\n", "0 0\n")
       end
 
-      it "puts coordinate out of board" do
-        expect(game).to receive(:puts).to be_include("coordinate out of board")
+      xit "puts coordinate out of board" do
+        expect(game).to receive(:puts).once
         game.player_turn_input
       end
     end
@@ -215,49 +222,53 @@ describe ConnectFour do
         allow(game).to receive(:gets).and_return("0 0\n")
       end
 
-      it "return array of x and y coordinate" do
+      xit "return array of x and y coordinate" do
         expect(game.player_turn_input).to eql([0, 0])
       end
     end
   end
 
   describe "#player_turn" do
+    before do
+      allow(game).to receive(:puts)
+    end
     context "when the board cell with this coordinate is already occupied, then is empty" do
+      let(:current_player) { 0 }
       before do
-        let(:current_player) { 0 }
         allow(game).to receive(:player_turn_input).and_return([0, 0], [1, 1])
+        game.instance_variable_set(:@player_name, ["", ""])
         game.instance_variable_set(:@board, [[1, nil], [nil, nil]])
         game.instance_variable_set(:@current_player, current_player)
       end
 
-      it "puts coordinate is already occupied" do
+      xit "puts coordinate is already occupied" do
         expect(game).to receive(:puts).at_least(:twice)
         game.player_turn
       end
 
-      it "call player_turn_input again" do
+      xit "call player_turn_input again" do
         expect(game).to receive(:player_turn_input).twice
         game.player_turn
       end
 
-      it "update board cell to current player" do
+      xit "update board cell to current player" do
         expect { game.player_turn }.to change {
-          game.instace_variable_get(:@board)[0][0]
+          game.instance_variable_get(:@board)[1][1]
         }.from(nil).to(current_player)
       end
 
-      it "change current player" do
+      xit "change current player" do
         expect { game.player_turn }.to change {
-          game.instace_variable_get(:@current_player)
+          game.instance_variable_get(:@current_player)
         }.from(current_player).to(1)
       end
     end
   end
 
   describe "#count_elements_in_row_from_point" do
+    let(:point) { [1, 1] }
     context "when board is empty" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [nil, nil, nil],
           [nil, nil, nil],
@@ -270,118 +281,109 @@ describe ConnectFour do
     end
     context "when board is full, besides center, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [0, 0, 0],
           [0, nil, 0],
           [0, 0, 0],
         ])
       end
-      it "return 0" do
+      xit "return 0" do
         expect(game.count_elements_in_row_from_point(point)).to be 0
       end
     end
     context "when board is empty, besides center, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [nil, nil, nil],
           [nil, 0, nil],
           [nil, nil, nil],
         ])
       end
-      it "return 1" do
+      xit "return 1" do
         expect(game.count_elements_in_row_from_point(point)).to be 1
       end
     end
     context "when board has horizontal line across center with length 3, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [nil, nil, nil],
           [0, 0, 0],
           [nil, nil, nil],
         ])
       end
-      it "return 3" do
+      xit "return 3" do
         expect(game.count_elements_in_row_from_point(point)).to be 3
       end
     end
     context "when board has vertical line across center with length 3, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [nil, 0, nil],
           [nil, 0, nil],
           [nil, 0, nil],
         ])
       end
-      it "return 3" do
+      xit "return 3" do
         expect(game.count_elements_in_row_from_point(point)).to be 3
       end
     end
     context "when board has left-right diagonal line across center with length 3, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [0, nil, nil],
           [nil, 0, nil],
           [nil, nil, 0],
         ])
       end
-      it "return 3" do
+      xit "return 3" do
         expect(game.count_elements_in_row_from_point(point)).to be 3
       end
     end
     context "when board has right-left diagonal line across center with length 3, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [nil, nil, 0],
           [nil, 0, nil],
           [0, nil, nil],
         ])
       end
-      it "return 3" do
+      xit "return 3" do
         expect(game.count_elements_in_row_from_point(point)).to be 3
       end
     end
     context "when board full by player 1 besides center by another player, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [0, 0, 0],
           [0, 1, 0],
           [0, 0, 0],
         ])
       end
-      it "return 1" do
+      xit "return 1" do
         expect(game.count_elements_in_row_from_point(point)).to be 1
       end
     end
     context "when board has horizontal line across center with length 5, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [0, 0, 0, 0, 0],
           [1, 1, 1, 1, 1],
           [0, 0, 0, 0, 0],
         ])
       end
-      it "return 5" do
+      xit "return 5" do
         expect(game.count_elements_in_row_from_point(point)).to be 5
       end
     end
     context "when board has 2 lines across center with length 5 and 3 respectively, point center" do
       before do
-        let(:point) { [1, 1] }
         game.instance_variable_set(:@board, [
           [0, 0, 1, 0, 0],
           [1, 1, 1, 1, 1],
           [0, 0, 1, 0, 0],
         ])
       end
-      it "return max is 5" do
+      xit "return max is 5" do
         expect(game.count_elements_in_row_from_point(point)).to be 5
       end
     end
@@ -395,16 +397,16 @@ describe ConnectFour do
         game.instace_variable_set(:@board, [[winner]])
       end
 
-      it "calls #count_elements_in_row_from_point once" do
+      xit "calls #count_elements_in_row_from_point once" do
         expect(game).to receive(:count_elements_in_row_from_point).once
         game.game_end?
       end
 
-      it "return true" do
+      xit "return true" do
         expect(game.game_end?).to be true
       end
 
-      it "set winner" do
+      xit "set winner" do
         expect { game.game_end? }.to change { game.instance_variable_get(:@winner) }.from(nil).to(winner)
       end
     end
@@ -414,16 +416,16 @@ describe ConnectFour do
         game.instace_variable_set(:@board, Array.new(3) { Array.new(3, nil) })
       end
 
-      it "calls 9 #count_elements_in_row_from_point" do
+      xit "calls 9 #count_elements_in_row_from_point" do
         expect(game).to receive(:count_elements_in_row_from_point).exactly(9).times
         game.game_end?
       end
 
-      it "return false" do
+      xit "return false" do
         expect(game.game_end?).to be false
       end
 
-      it "don't change winner" do
+      xit "don't change winner" do
         expect { game.game_end? }.to_not change { game.instance_variable_get(:@winner) }
       end
     end
@@ -433,16 +435,16 @@ describe ConnectFour do
         game.instace_variable_set(:@board, Array.new(3) { Array.new(3, 0) })
       end
 
-      it "calls 9 #count_elements_in_row_from_point" do
+      xit "calls 9 #count_elements_in_row_from_point" do
         expect(game).to receive(:count_elements_in_row_from_point).exactly(9).times
         game.game_end?
       end
 
-      it "return true" do
+      xit "return true" do
         expect(game.game_end?).to be true
       end
 
-      it "don't change winner" do
+      xit "don't change winner" do
         expect { game.game_end? }.to_not change { game.instance_variable_get(:@winner) }
       end
     end
